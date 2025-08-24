efbb1989dc66c8962a05ca5105e66d46fd8907dc
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-25 02:08:44 +0800

    Remove build-manifest.xml generation
    Refer to I1a92a171f2e2ec1ba2453d6c666cefcd9884b39b.
    
    This reverts commits 8ed354004a5c7511d595bd1f76fb3bed761de525 and 396fc5805c10016d3d9413ef4c769e7e50ff1bd3.
    
    Change-Id: I7f854e91e92d797f342af0edbdbddbe0e4390485

--- envsetup.sh
+++ envsetup.sh
@@ -579,11 +579,6 @@ function lunch()
     $T/prebuilts/build-tools/linux-x86/bin/py3-cmd $T/vendor/lumine/build/tools/barista.py $product
     source_vendorsetup &>/dev/null
 
-    # Generate build-manifest.xml after roomservice finishes
-    cd $T > /dev/null
-    vendor/lineage/build/tools/gen_build-manifest.sh
-    cd - > /dev/null
-
     # Validate the selection and set all the environment stuff
     _lunch_meat $product $release $variant
 
